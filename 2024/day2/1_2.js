374624733a39349d3829cc7286a0d37052f20bf0
Diaresta <TheAndrewRobles@yahoo.com>
2024-12-02 23:52:53 -0500

    2024 - Day 2 - Part 2

---
+++ 2024/day2/1_2.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'input.txt');
+
+const INPUT = fs.readFileSync(filePath, 'utf-8');
+const LIST_LINES = INPUT.trim().split('\n');
+
+let output = 0;
+let levels = LIST_LINES.map((i) => i.split(/\s+/).map(Number));
+let isDecreasing;
+let distance;
+
+const isLevelSafe = () => {
+  levels.forEach((level) => {
+    for (let i = 0; i < level.length; i++) {
+      let removeIndex = [...level.slice(0, i), ...level.slice(i + 1)];
+
+      if (isDampened(removeIndex)) {
+        output++;
+        break;
+      }
+    }
+  });
+
+  return output;
+};
+
+let isDampened = (input) => {
+  isDecreasing = input[1] < input[0];
+
+  return input.slice(1).every((level, i) => {
+    distance = level - input[i];
+
+    return (
+      distance !== 0 &&
+      distance >= -3 &&
+      distance <= 3 &&
+      isDecreasing === distance < 0
+    );
+  });
+};
+
+console.log(isLevelSafe(LIST_LINES));
